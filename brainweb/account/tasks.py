f5c1cc6ea2169a5c909e79f87c79e1b22518a3f0
Harun Altun <80642843+hrnaltun@users.noreply.github.com>
2024-06-29 20:21:16 +0300

    çoklu servis eklendi. servis modeli değiştirildi. tasks.py değiştirildi

--- brainweb/account/tasks.py
+++ brainweb/account/tasks.py
@@ -5,71 +5,169 @@ import os
 import importlib
 from django.conf import settings
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 @shared_task
 def process_uploaded_file(uploaded_file_id, servis_id):
-    uploaded_file_obj = UploadedFile.objects.get(id=uploaded_file_id)
-    file_path = uploaded_file_obj.file.path
-
-    # Get the Servis object to check if PDF generation is required
     try:
+        uploaded_file_obj = UploadedFile.objects.get(id=uploaded_file_id)
         servis = Servis.objects.get(id=servis_id)
-    except Servis.DoesNotExist:
-        uploaded_file_obj.processing_status = "Başarısız"
-        uploaded_file_obj.save()
-        return f"Servis bulunamadı: {servis_id}"
+        file_path = uploaded_file_obj.file.path
 
-    # Determine the model path based on servis_id
-    model_dir = os.path.join(BASE_DIR, 'media', 'model_files', str(servis_id))
-    model_path = os.path.join(model_dir, 'model.pth')
+        results = []
 
-    filename_without_extension = os.path.splitext(uploaded_file_obj.file.name)[0]
-    output_filename = f"{filename_without_extension}_output.nii.gz"
-    output_path = os.path.join(settings.MEDIA_ROOT, 'outputs', output_filename)
+        if servis.çoklu_servis and servis.çalıştırılacak_servisler:
+            # Çalıştırılacak servislerin ID'lerini ayırıp liste haline getir
+            run_service_ids = [int(sid.strip()) for sid in servis.çalıştırılacak_servisler.split(',') if sid.strip()]
 
-    # Dynamically import the module and call the main function
-    module_path = f"media.model_files.{servis_id}.model"
-    try:
-        model_module = importlib.import_module(module_path)
-        if servis.pdf_oluştur:
-            pdf_output_filename = f"{filename_without_extension}_output.pdf"
-            pdf_output_path = os.path.join(settings.MEDIA_ROOT, 'outputs', pdf_output_filename)
+            for run_servis_id in run_service_ids:
+                # Her bir servis için işlem yap
+                model_dir = os.path.join(settings.BASE_DIR, 'media', 'model_files', str(run_servis_id))
+                model_path = os.path.join(model_dir, 'model.pth')
 
-            if os.path.exists(model_path):
-                result, result_pdf = model_module.main(file_path, model_path, output_path, pdf_output_path)
-            else:
-                result, result_pdf = model_module.main(file_path, output_path, pdf_output_path)
+                filename_without_extension = os.path.splitext(uploaded_file_obj.file.name)[0]
+                output_filename = f"{filename_without_extension}_{run_servis_id}_output.nii.gz"
+                output_path = os.path.join(settings.MEDIA_ROOT, 'outputs', output_filename)
+
+                module_path = f"media.model_files.{run_servis_id}.model"
+                try:
+                    model_module = importlib.import_module(module_path)
+                    if not hasattr(model_module, 'main'):
+                        uploaded_file_obj.processing_status = "Başarısız"
+                        uploaded_file_obj.save()
+                        return f"Modülde 'main' fonksiyonu bulunamadı: {module_path}"
+                    run_servis = Servis.objects.get(id=run_servis_id)
+                    if run_servis.pdf_oluştur:
+                        print("çalışıyor2")
+                        pdf_output_filename = f"{filename_without_extension}_{run_servis_id}_output.pdf"
+                        pdf_output_path = os.path.join(settings.MEDIA_ROOT, 'outputs', pdf_output_filename)
+                        print("çalışıyor")
+                        if os.path.exists(model_path):
+                            result, _ = model_module.main(file_path, model_path, output_path, pdf_output_path)
+                        else:
+                            result, _ = model_module.main(file_path, output_path, pdf_output_path)
+                    else:
+                        if os.path.exists(model_path):
+                            result, _ = model_module.main(file_path, model_path, output_path)
+                        else:
+                            result, _ = model_module.main(file_path, output_path)
+
+                    results.append(result)
+
+                except ModuleNotFoundError:
+                    uploaded_file_obj.processing_status = "Başarısız"
+                    uploaded_file_obj.save()
+                    return f"Model modülü bulunamadı: {module_path}"
+                except AttributeError:
+                    uploaded_file_obj.processing_status = "Başarısız"
+                    uploaded_file_obj.save()
+                    return f"Modülde 'main' fonksiyonu bulunamadı: {module_path}"
+                except Exception as e:
+                    uploaded_file_obj.processing_status = "Başarısız"
+                    uploaded_file_obj.save()
+                    return f"Bir hata oluştu: {str(e)}"
+
+                uploaded_file_obj.save()
+
+            model_dir = os.path.join(settings.BASE_DIR, 'media', 'model_files', str(servis_id))
+            model_path = os.path.join(model_dir, 'model.pth')
+
+            filename_without_extension = os.path.splitext(uploaded_file_obj.file.name)[0]
+            output_filename = f"{filename_without_extension}_output.nii.gz"
+            output_path = os.path.join(settings.MEDIA_ROOT, 'outputs', output_filename)
+            module_path = f"media.model_files.{servis_id}.model"
+
+            try:
+                model_module = importlib.import_module(module_path)
+                if not hasattr(model_module, 'main'):
+                    uploaded_file_obj.processing_status = "Başarısız"
+                    uploaded_file_obj.save()
+                    return f"Modülde 'main' fonksiyonu bulunamadı: {module_path}"
+                
+                if servis.pdf_oluştur:
+                    pdf_output_filename = f"{filename_without_extension}_output.pdf"
+                    pdf_output_path = os.path.join(settings.MEDIA_ROOT, 'outputs', pdf_output_filename)
+                    if os.path.exists(model_path):
+                        result, result_pdf = model_module.main(results, model_path, output_path, pdf_output_path)
+                    else:
+                        result, result_pdf = model_module.main(results, output_path, pdf_output_path)
+                else:
+                    if os.path.exists(model_path):
+                        result = model_module.main(results, model_path, output_path)
+                    else:
+                        result = model_module.main(results, output_path)
+
+            except ModuleNotFoundError:
+                uploaded_file_obj.processing_status = "Başarısız"
+                uploaded_file_obj.save()
+                return f"Model modülü bulunamadı: {module_path}"
+            except AttributeError:
+                uploaded_file_obj.processing_status = "Başarısız"
+                uploaded_file_obj.save()
+                return f"Modülde 'main' fonksiyonu bulunamadı: {module_path}"
+            except Exception as e:
+                uploaded_file_obj.processing_status = "Başarısız"
+                uploaded_file_obj.save()
+                return f"Bir hata oluştu: {str(e)}"
+
+            uploaded_file_obj.output = result
+            uploaded_file_obj.processing_status = "Tamamlandı"
+            if servis.pdf_oluştur and result_pdf and os.path.exists(result_pdf):
+                uploaded_file_obj.output_pdf = result_pdf
+            uploaded_file_obj.save()
+            return "Görev başarıyla tamamlandı"
 
         else:
+            # Normal işlem devam ediyor
+            model_dir = os.path.join(settings.BASE_DIR, 'media', 'model_files', str(servis_id))
+            model_path = os.path.join(model_dir, 'model.pth')
 
-            if os.path.exists(model_path):
-                result = model_module.main(file_path, model_path, output_path)
-            else:
-                result = model_module.main(file_path, output_path)
+            filename_without_extension = os.path.splitext(uploaded_file_obj.file.name)[0]
+            output_filename = f"{filename_without_extension}_output.nii.gz"
+            output_path = os.path.join(settings.MEDIA_ROOT, 'outputs', output_filename)
 
-    except ModuleNotFoundError:
-        uploaded_file_obj.processing_status = "Başarısız"
-        uploaded_file_obj.save()
-        return f"Model modülü bulunamadı: {module_path}"
-    except AttributeError:
-        uploaded_file_obj.processing_status = "Başarısız"
-        uploaded_file_obj.save()
-        return f"Modülde 'main' fonksiyonu bulunamadı: {module_path}"
-    except Exception as e:
-        uploaded_file_obj.processing_status = "Başarısız"
-        uploaded_file_obj.save()
-        return f"Bir hata oluştu: {str(e)}"
+            module_path = f"media.model_files.{servis_id}.model"
+            try:
+                model_module = importlib.import_module(module_path)
+                if not hasattr(model_module, 'main'):
+                    uploaded_file_obj.processing_status = "Başarısız"
+                    uploaded_file_obj.save()
+                    return f"Modülde 'main' fonksiyonu bulunamadı: {module_path}"
+                
+                if servis.pdf_oluştur:
+                    pdf_output_filename = f"{filename_without_extension}_output.pdf"
+                    pdf_output_path = os.path.join(settings.MEDIA_ROOT, 'outputs', pdf_output_filename)
 
-    # Check if the result path exists
-    if result and os.path.exists(result):
-        uploaded_file_obj.output = result
-        uploaded_file_obj.processing_status = "Tamamlandı"
-        if servis.pdf_oluştur and result_pdf and os.path.exists(result_pdf):
-            uploaded_file_obj.output_pdf = result_pdf
-        uploaded_file_obj.save()
-        return "Görev başarıyla tamamlandı"
-    else:
-        uploaded_file_obj.processing_status = "Başarısız"
-        uploaded_file_obj.save()
-        return f"Çıktı üretilemedi, dosya bulunamadı: {result}"
+                    if os.path.exists(model_path):
+                        result, result_pdf = model_module.main(file_path, model_path, output_path, pdf_output_path)
+                    else:
+                        result, result_pdf = model_module.main(file_path, output_path, pdf_output_path)
+                else:
+                    if os.path.exists(model_path):
+                        result = model_module.main(file_path, model_path, output_path)
+                    else:
+                        result = model_module.main(file_path, output_path)
+
+            except ModuleNotFoundError:
+                uploaded_file_obj.processing_status = "Başarısız"
+                uploaded_file_obj.save()
+                return f"Model modülü bulunamadı: {module_path}"
+            except AttributeError:
+                uploaded_file_obj.processing_status = "Başarısız"
+                uploaded_file_obj.save()
+                return f"Modülde 'main' fonksiyonu bulunamadı: {module_path}"
+            except Exception as e:
+                uploaded_file_obj.processing_status = "Başarısız"
+                uploaded_file_obj.save()
+                return f"Bir hata oluştu: {str(e)}"
+
+            # uploaded_file_obj içine sonucu kaydet
+            uploaded_file_obj.output = result
+            uploaded_file_obj.processing_status = "Tamamlandı"
+            if servis.pdf_oluştur and result_pdf and os.path.exists(result_pdf):
+                uploaded_file_obj.output_pdf = result_pdf
+            uploaded_file_obj.save()
+            return "Görev başarıyla tamamlandı"
+
+    except UploadedFile.DoesNotExist:
+        return f"Yüklenen dosya bulunamadı: {uploaded_file_id}"
+    except Servis.DoesNotExist:
+        return f"Servis bulunamadı: {servis_id}"
--- brainweb/guest/models.py
+++ brainweb/guest/models.py
@@ -14,9 +14,11 @@ class Servis(models.Model):
     açıklama_uzun = models.TextField()
     sayfadaki_sırası = models.IntegerField()
     aktif_pasif = models.BooleanField(default=True)
-    model_py_dosyası = models.FileField(upload_to=get_upload_path, default='', blank=True, null=True)  # Model Python dosyasını saklamak için
-    model_pth_dosyası = models.FileField(upload_to=get_upload_path, default='', blank=True, null=True)  # Model .pth dosyasını saklamak için
-    pdf_oluştur = models.BooleanField(default=False)  # PDF çıktısı verilip verilmeyeceğini belirler
+    model_py_dosyası = models.FileField(upload_to=get_upload_path, default='', blank=True, null=True)
+    model_pth_dosyası = models.FileField(upload_to=get_upload_path, default='', blank=True, null=True)
+    pdf_oluştur = models.BooleanField(default=False)
+    çoklu_servis = models.BooleanField(default=False)
+    çalıştırılacak_servisler = models.TextField(blank=True, null=True)  # Bu alana virgülle ayrılmış servis ID'leri saklanacak
     objects = models.Manager()
 
     def __str__(self):
@@ -31,27 +33,23 @@ class Servis(models.Model):
                 old_py_file = old_service.model_py_dosyası.path if old_service.model_py_dosyası else None
                 old_pth_file = old_service.model_pth_dosyası.path if old_service.model_pth_dosyası else None
 
-                # Dosyaların değiştirilip değiştirilmediğini kontrol et
                 new_py_file = self.model_py_dosyası.file if self.model_py_dosyası else None
                 new_pth_file = self.model_pth_dosyası.file if self.model_pth_dosyası else None
 
                 super().save(*args, **kwargs)
 
-                # Eğer model_py_dosyası değiştiyse eski dosyayı sil
                 if old_py_file and (not new_py_file or new_py_file.name != old_py_file):
                     try:
                         os.remove(old_py_file)
                     except FileNotFoundError:
                         pass
 
-                # Eğer model_pth_dosyası değiştiyse eski dosyayı sil
                 if old_pth_file and (not new_pth_file or new_pth_file.name != old_pth_file):
                     try:
                         os.remove(old_pth_file)
                     except FileNotFoundError:
                         pass
 
-                # Eski dosyalar klasörü boşsa klasörü sil
                 old_path = os.path.join(settings.MEDIA_ROOT, 'model_files', str(old_service.pk))
                 if os.path.exists(old_path) and not os.listdir(old_path):
                     rmtree(old_path)
